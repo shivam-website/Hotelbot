ac0689e238ff09d0bb7eeb20996ca058c881be98
shivam-website <shivamsah1222@gmail.com>
2025-06-26 18:56:24 +0545

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // Use Render's PORT env var, or default to 5000
 const DB_FILE = path.join(__dirname, 'orders.json');
 
 app.use(cors());
